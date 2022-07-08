228b97900765d9361b817765eef2527ec34c6935
Nghi1100 <vovannghi20@gmail.com>
2022-07-08 18:56:32 +0700

    five-commit

--- next.config.js
+++ next.config.js
@@ -10,9 +10,9 @@ const nextConfig = {
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-      config.resolve.fallback = {
-        fs: false,
+      // set 'fs' to an empty module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.node = {
+        fs: "empty",
       };
     }
 
