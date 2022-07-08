14ae541ae2e6b6e3fbbecfb8da85aecccf4a41fb
Nghi1100 <vovannghi20@gmail.com>
2022-07-08 19:12:46 +0700

    eight-commit

--- components/Post.js
+++ components/Post.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/Link";
+import Link from "next/link";
 import Image from "next/image";
 import styles from "./post.module.css";
 const Post = ({ post, home }) => {
