8beb4fdd0cca67ba13d328c98501304cd45213ba
Nghi1100 <vovannghi20@gmail.com>
2022-07-08 18:53:14 +0700

    four-commit

--- next.config.js
+++ next.config.js
@@ -8,6 +8,16 @@ const nextConfig = {
   env: {
     URL_API: "http://localhost:1337",
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
